90bf4301259e76ee11b2ba52e8fa6b5295d4799b
giuliopft <giulio.ticli04@gmail.com>
2020-12-31 18:30:34 +0100

    basic match management

--- src/app.mjs
+++ src/app.mjs
@@ -5,6 +5,8 @@ import express from 'express';
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const app = express();
 const port = 41726;
+const users = new Set();
+export const games = new Set();
 
 app.use(express.static(__dirname + '/public'));
 
--- src/backend/game.mjs
+++ src/backend/game.mjs
@@ -2,15 +2,94 @@
 
 import { BlackCard, WhiteCard } from './card.mjs'
 import { User } from './user.mjs'
+import { games } from '../app.mjs'
 
-try {
-    let card1 = new BlackCard(`alpha`);
-} catch (e) {
-    console.log(e);
+const protoSettings = {
+    whiteCardsPerPlayer = 10,
 }
-let card2 = new BlackCard(`alpha ___`);
-let user = new User(`Giulio`, null);
-let user2 = new User(`Giulio`, null);
-let user3 = new User(`Giulio`, null);
-let user4 = new User(`Giulio`, null);
-console.log(`${user.id}, ${user2.id}, ${user3.id}, ${user4.id}`)
+
+class Player {
+    constructor(user, points, whiteCards) {
+        this._user = user;
+        this.points = points;
+        this._whiteCards = whiteCards;
+    }
+
+    get user() {
+        return this._user;
+    }
+
+    get points() {
+        return this.points;
+    }
+
+    set points(points) {
+        this.points = points;
+    }
+
+    get whiteCards() {
+        return this._whiteCards;
+    }
+
+    addWhiteCards(...whiteCards) {
+        this._whiteCards.push(whiteCards);
+    }
+}
+
+class Round {
+    constructor(czar,) {
+        //TODO Create here
+    }
+}
+
+class Game {
+    constructor(admin, players, blackCards, whiteCards, settings) {
+        this._admin = admin;
+        this._players = players;
+        this._blackCards = blackCards;
+        this._whiteCards = whiteCards;
+        this._settings = settings;
+        this._id = this._generateId();
+    }
+
+    get admin() {
+        return this._admin;
+    }
+
+    get players() {
+        return this._players;
+    }
+
+    get blackCards() {
+        return this._blackCards;
+    }
+
+    get whiteCards() {
+        return this._whiteCards;
+    }
+
+    get settings() {
+        return this._settings;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    _generateId() {
+        let id = ``;
+        for (let i = 0; i < 8; i++) {
+            id += String.fromCodePoint(Math.floor(Math.random() * 26 + 65));
+        }
+        for (let game in games) {
+            if (game.id.valueOf() === id.valueOf()) {
+                return this._generateId();
+            }
+        }
+        return id;
+    }
+
+    nextRound() {
+        //TODO This
+    }
+}
