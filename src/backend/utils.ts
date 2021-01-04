c65a27e2b4adfbe0559503c459e50a5a763c6965
giuliopft <giulio.ticli04@gmail.com>
2021-01-04 18:25:36 +0100

    toString() in RGBColor

--- src/backend/utils.ts
+++ src/backend/utils.ts
@@ -5,11 +5,14 @@ export class RGBColor {
     readonly g: number;
     readonly b: number;
     constructor(r: number, g: number, b: number) {
-        if (r < 0 || r > 0xFF || g < 0 || g > 0xFF || b < 0  || b > 0xFF) {
+        if (r < 0 || r > 0xFF || g < 0 || g > 0xFF || b < 0 || b > 0xFF) {
             throw new Error("RGB colors must have R, G and B values between 0 and 255 (0xFF)")
         }
         this.r = r;
         this.g = g;
         this.b = b;
+    }
+    toString(): string {
+        return `#${this.r < 16 ? 0 : ''}${this.r.toString(16)}${this.g < 16 ? 0 : ''}${this.g.toString(16)}${this.b < 16 ? 0 : ''}${this.b.toString(16)}`
     }
 }
